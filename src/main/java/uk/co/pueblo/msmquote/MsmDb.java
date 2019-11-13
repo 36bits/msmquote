ca3eb47f0a64591425dba309b882c15760a65c07
Jonathan Casiot <jonathan@pueblo.co.uk>
2019-11-13 23:17:23 +0000

    Clean Up Ignored Files Part II

---
+++ src/main/java/uk/co/pueblo/msmquote/MsmDb.java
@@ -0,0 +1,63 @@
+package uk.co.pueblo.msmquote;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.healthmarketscience.jackcess.CryptCodecProvider;
+import com.healthmarketscience.jackcess.Database;
+import com.healthmarketscience.jackcess.DatabaseBuilder;
+
+public class MsmDb extends DatabaseBuilder {
+	private static final Logger LOGGER = LogManager.getLogger(MsmDb.class);
+	
+	private Database db;
+		
+	// Constructor
+	public MsmDb(String fileName, String password) throws IOException {
+		
+		// Create lock file
+		String lockFileName = null;
+		int i = fileName.lastIndexOf('.');
+        if (i <= 0) {
+            lockFileName = fileName;
+        }
+        else {
+        	lockFileName = fileName.substring(0, i);
+        }
+		File lockFile = new File(lockFileName + ".lrd");
+		LOGGER.info("Creating lock file: {}", lockFile.getAbsolutePath());
+		if (!lockFile.createNewFile()) {
+			throw new FileAlreadyExistsException("Lock file already exists");
+		}
+		lockFile.deleteOnExit();
+		
+		// Open Money database
+		File dbFile = new File(fileName);
+		CryptCodecProvider cryptCp = null;
+				
+		if (password == null) {
+			cryptCp = new CryptCodecProvider();
+		}
+		else {
+			cryptCp = new CryptCodecProvider(password);
+		}
+		LOGGER.info("Opening Money file: {}", dbFile.getAbsolutePath());
+		db = new DatabaseBuilder(dbFile).setCodecProvider(cryptCp).open();		
+		return;
+	}
+		
+	public Database getDb() {
+	return db;
+	}
+
+	public void closeDb() throws IOException {
+		LOGGER.info("Closing Money file: {}", db.getFile());
+		db.close();
+		return;
+	}
+}
---
+++ src/main/java/uk/co/pueblo/msmquote/Update.java
@@ -0,0 +1,98 @@
+package uk.co.pueblo.msmquote;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.healthmarketscience.jackcess.Database;
+
+public class Update {
+    private static final Logger LOGGER = LogManager.getLogger(Update.class);
+	
+    // Define exit code fields
+    private static final int OK = 0;
+    private static final int WARNING = 1;
+    private static final int ERROR = 2;
+        
+    public static void main(String[] args) {
+    	
+    	Instant startTime = Instant.now();
+		
+		// Get arguments
+    	String password = null;
+    	String source = null;
+	    
+    	if (args.length == 3) {
+	    	password = args[1];
+	    	source = args[2];
+    	} else if (args.length == 2) {
+    		source = args[1];
+    	} else {
+	    	System.out.printf("Usage: %s filename [password] source", Update.class.getName());
+	        return;
+	    }    	
+
+    	int exitCode = OK; 
+    	
+    	// Open Money database
+    	MsmDb db = null;
+    	Database openedDb = null;
+		try {
+			db = new MsmDb(args[0], password);
+			openedDb = db.getDb();
+		} catch (Exception e) {
+			LOGGER.fatal(e);
+			System.exit(ERROR);
+		}
+    	
+		// Process quote data
+		int hsec;
+		Map<String, Object> quoteRow = new HashMap<>();
+		MsmSecTable secTable = null;
+		MsmSpTable spTable = null;
+		MsmFxTable fxTable = null;
+		
+		try {
+			secTable = new MsmSecTable(openedDb);
+			spTable = new MsmSpTable(openedDb);
+			fxTable = new MsmFxTable(openedDb);
+			YahooQuote yahooQuote = new YahooQuote(source);
+			while (true) {
+				if ((quoteRow = yahooQuote.getNext()) == null) {
+					break;
+				}
+				if (quoteRow.get("rate") == null) {
+					if((hsec = secTable.update(quoteRow)) != -1) {
+						spTable.update(quoteRow, hsec);
+					} else {
+						exitCode = WARNING;
+					}
+				} else {		
+					if (!fxTable.update(quoteRow)) {
+						exitCode = WARNING;
+					}
+				}
+			}
+		} catch (IOException e) {
+			LOGGER.fatal(e);
+			exitCode = ERROR;
+		} finally {
+			// Add any new rows to the SP table and close the Money database
+			try {
+				spTable.addNewRows();
+				db.closeDb();
+			} catch (IOException e) {
+				LOGGER.fatal(e);
+				exitCode = ERROR;
+			}
+		}
+					
+        LOGGER.info("Duration: {}", Duration.between(startTime, Instant.now()).toString());
+       	System.exit(exitCode);
+   }
+}
