8ebbc4c917a12dc00a517e6f80ec5258fc0710f9
Jonathan Casiot <jonathan@pueblo.co.uk>
2020-06-02 21:00:14 +0100

    Add support for Portfolio status bar index symbol translation

--- src/main/java/uk/co/pueblo/msmquote/source/YahooUtil.java
+++ src/main/java/uk/co/pueblo/msmquote/source/YahooUtil.java
@@ -3,6 +3,8 @@ package uk.co.pueblo.msmquote.source;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Properties;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -34,14 +36,18 @@ class YahooUtil {
 	 * Generate Yahoo symbol from Money symbol.
 	 * 
 	 * @param	symbol			the Money symbol for the security
-	 * @param	exchangeCode	the Yahoo exchange code for the security
+	 * @param	country			the Money country for the security
+	 * @param	props			the YahooQuote properties
 	 * @return					the equivalent Yahoo symbol
 	 */
-	static String getYahooSymbol(String symbol, String exchangeCode) {
-		String yahooSymbol = null;
-		if (symbol.matches("(.*\\..$|.*\\...$|^\\^.*)")) {
-			// Symbol is in Yahoo format
-			yahooSymbol = symbol;
+	static String getYahooSymbol(String symbol, String country, Properties props) {
+		String yahooSymbol = symbol;
+		String prop;
+		if (symbol.matches("^\\$US:.*")) {
+			// Symbol is in Money index format '$US:symbol'
+			if ((prop = props.getProperty("index." + symbol.substring(4))) != null) {
+				yahooSymbol = prop;
+			}
 		} else if (symbol.matches("^\\$..:.*")) {
 			// Symbol is in Money index format '$xx:symbol'
 			yahooSymbol = "^" + symbol.substring(4);							
@@ -50,13 +56,13 @@ class YahooUtil {
 			yahooSymbol = "^" + symbol.substring(1);
 		} else if (symbol.matches("^..:.*")) {
 			// Symbol is in Money security format 'xx:symbol'
-			if (exchangeCode != null) {
-				yahooSymbol = symbol.substring(3) + exchangeCode;
+			if ((prop = props.getProperty("exchange." + country)) != null) {
+				yahooSymbol = symbol.substring(3) + prop;
 			}
 		} else {
-			// Symbol is in Money security format for default currency 'symbol'
-			if (exchangeCode != null) {
-				yahooSymbol = symbol + exchangeCode; 
+			// Symbol is in Money un-prefixed security format 'symbol'
+			if ((prop = props.getProperty("exchange." + country)) != null) {
+				yahooSymbol = symbol + prop; 
 			}
 		}		
 		return yahooSymbol;
