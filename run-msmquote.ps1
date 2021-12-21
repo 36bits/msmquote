9aff8dc98f606a516c008f988dd153ec9aaa34f4
Jonathan Casiot <jonathan@pueblo.co.uk>
2021-12-21 14:52:42 +0000

    Improve log messages

--- run-msmquote.ps1
+++ run-msmquote.ps1
@@ -9,19 +9,20 @@ if ($PSVersionTable.PSVersion.Major -ne 5) {
     return
 }
 
+$mnyFile = (Get-Item $mnyFile).FullName
 $log = $env:UserProfile + "\log\msmquote.log"
 $jre = $env:ProgramFiles + "\msmquote\bin\java.exe"
-$jar = $env:ProgramFiles + "\msmquote\msmquote-4.0.0-SNAPSHOT.jar"
+$jar = $env:ProgramFiles + "\msmquote\msmquote-4.0.2.jar"
 $jreOpts = @()
 
-Write-Eventlog -LogName Application -Source msmquote -EntryType Information -EventId 1000 -Category 0 -Message "Update started. File: $mnyFile."
+Write-Eventlog -LogName Application -Source msmquote -EntryType Information -EventId 1000 -Category 0 -Message "Update started: file = $mnyFile."
 & $jre $jreOpts -jar $jar $mnyFile $mnyPswd $source *>> $log
 $savedExitCode = $lastexitcode
 
 if ($savedExitCode -eq 1) {
-    Write-Eventlog -LogName Application -Source msmquote -EntryType Warning -EventId 1001 -Category 0 -Message "Update completed with warnings (exit code $savedExitCode). Please review the log file: $log."
+    Write-Eventlog -LogName Application -Source msmquote -EntryType Warning -EventId 1001 -Category 0 -Message "Update completed with warnings: file = $mnyFile, exit code = $savedExitCode."
 } elseif ($savedExitCode -eq 2) {
-    Write-Eventlog -LogName Application -Source msmquote -EntryType Error -EventId 1001 -Category 0 -Message "Update completed with errors (exit code $savedExitCode). Please review the log file: $log."
+    Write-Eventlog -LogName Application -Source msmquote -EntryType Error -EventId 1001 -Category 0 -Message "Update completed with errors: file = $mnyFile, exit code = $savedExitCode."
 } else {
-    Write-Eventlog -LogName Application -Source msmquote -EntryType Information -EventId 1001 -Category 0 -Message "Update completed successfully."
+    Write-Eventlog -LogName Application -Source msmquote -EntryType Information -EventId 1001 -Category 0 -Message "Update completed successfully: file = $mnyFile, exit code = $savedExitCode."
 }
