24e9c90062e82146c62d97c0b00ecca1b60c5564
Jonathan Casiot <jonathan@pueblo.co.uk>
2022-01-22 14:32:31 +0000

    Relocate program and log files

--- run-msmquote.ps1
+++ run-msmquote.ps1
@@ -10,9 +10,9 @@ if ($PSVersionTable.PSVersion.Major -ne 5) {
 }
 
 $mnyFile = (Get-Item $mnyFile).FullName
-$log = $env:UserProfile + "\log\msmquote.log"
-$jre = $env:ProgramFiles + "\msmquote\bin\java.exe"
-$jar = $env:ProgramFiles + "\msmquote\msmquote-4.1.0.jar"
+$log = $env:LOCALAPPDATA + "\log\msmquote.log"
+$jre = $env:LOCALAPPDATA + "\Programs\msmquote\bin\java.exe"
+$jar = $env:LOCALAPPDATA + "\Programs\msmquote-4.1.0.jar"
 $jreOpts = @()
 
 Write-Eventlog -LogName Application -Source msmquote -EntryType Information -EventId 1000 -Category 0 -Message "Update started.`nFile = $mnyFile"
