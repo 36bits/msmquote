aa64da870e142550579fe918b86da203e5a141f1
Jonathan Casiot <jonathan@pueblo.co.uk>
2022-05-23 10:16:49 +0100

    Add URL connection and read timeouts

--- src/main/java/uk/co/pueblo/msmquote/YahooSource.java
+++ src/main/java/uk/co/pueblo/msmquote/YahooSource.java
@@ -3,6 +3,7 @@ package uk.co.pueblo.msmquote;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,6 +15,8 @@ abstract class YahooSource implements QuoteSource {
 
 	// Constants
 	static final Logger LOGGER = LogManager.getLogger(YahooSource.class);
+	static final int CONNECT_TIMEOUT = 10000;	// milliseconds
+	static final int READ_TIMEOUT = 60000;		// milliseconds
 
 	YahooSource(String propsFile) {
 		// Open properties
@@ -35,8 +38,12 @@ abstract class YahooSource implements QuoteSource {
 	 * @throws IOException
 	 */
 	static JsonNode getJson(String apiUrl) throws IOException {
+		URL url = new URL(apiUrl);
+		URLConnection con = url.openConnection();
+		con.setConnectTimeout(CONNECT_TIMEOUT);
+		con.setReadTimeout(READ_TIMEOUT);		
 		LOGGER.info("Requesting quote data from Yahoo Finance API");
-		try (InputStream quoteIs = new URL(apiUrl).openStream();) {		// using try-with-resources to get AutoClose of InputStream
+		try (InputStream quoteIs = con.getInputStream();) {		// using try-with-resources to get AutoClose of InputStream
 			ObjectMapper mapper = new ObjectMapper();
 			return mapper.readTree(quoteIs);
 		}
