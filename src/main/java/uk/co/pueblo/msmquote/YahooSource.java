64ff96617624103ef7d8594071520040978cee40
Jonathan Casiot <jonathan@pueblo.co.uk>
2022-06-14 14:27:04 +0100

    Fix URL build for Yahoo Finance historical quotes API

--- src/main/java/uk/co/pueblo/msmquote/YahooSource.java
+++ src/main/java/uk/co/pueblo/msmquote/YahooSource.java
@@ -3,11 +3,11 @@ package uk.co.pueblo.msmquote;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-import java.net.URLEncoder;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,7 +18,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 abstract class YahooSource implements QuoteSource {
 
 	// Constants
-	static final Logger LOGGER = LogManager.getLogger(YahooSource.class);	
+	static final Logger LOGGER = LogManager.getLogger(YahooSource.class);
 
 	YahooSource(String propsFile) {
 		// Open properties
@@ -39,23 +39,44 @@ abstract class YahooSource implements QuoteSource {
 	 * @return the quote data
 	 * @throws IOException
 	 * @throws InterruptedException
+	 * @throws URISyntaxException 
 	 */
-	static JsonNode getJson(String url) throws InterruptedException, IOException {		
+	static JsonNode getJson(String url) throws InterruptedException, IOException, URISyntaxException {
 		// Get http timeout from properties file
 		String prop;
 		int httpTimeout = ((prop = PROPS.getProperty("api.http.timeout")) != null) ? Integer.parseInt(prop) : 20;
-				
+
 		// Build and send http request
-		String[] urlSplit = url.split("(?<=\\=)", 2);	// Split parameters out of URL		
+		URL apiUrl = new URL(url);
+		URI apiUri = new URI(apiUrl.getProtocol(), apiUrl.getUserInfo(), apiUrl.getHost(), apiUrl.getPort(), apiUrl.getPath(), apiUrl.getQuery(), apiUrl.getRef());
+		LOGGER.debug(apiUri.toASCIIString());
 		HttpClient httpClient = HttpClient.newHttpClient();
-		URI uri = URI.create(urlSplit[0] + URLEncoder.encode(urlSplit[1], StandardCharsets.UTF_8.toString()));
-		HttpRequest request = HttpRequest.newBuilder(uri).timeout(Duration.ofSeconds(httpTimeout)).GET().build();
-		
+		HttpRequest request = HttpRequest.newBuilder(apiUri).timeout(Duration.ofSeconds(httpTimeout)).GET().build();
+
 		LOGGER.info("Requesting quote data from Yahoo Finance API, request timeout = {}s", httpTimeout);
 		HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 		LOGGER.info("Received {} bytes of quote data", response.body().length());
-		
+
 		ObjectMapper mapper = new ObjectMapper();
 		return mapper.readTree(response.body());
+	}
+
+	static int getDivisor(String quoteCurrency, String quoteType) {
+		String prop;
+		return ((prop = PROPS.getProperty("divisor." + quoteCurrency + "." + quoteType)) == null) ? 1 : Integer.parseInt(prop);
+	}
+
+	static int getMultiplier(String quoteCurrency, String quoteType) {
+		String prop;
+		return ((prop = PROPS.getProperty("multiplier." + quoteCurrency + "." + quoteType)) == null) ? 100 : Integer.parseInt(prop);
+	}
+
+	static String quoteAdjust(String value, String adjuster, int divisor, int multiplier) {
+		if (adjuster.equals("d")) {
+			value = String.valueOf(Double.parseDouble(value) / divisor);
+		} else if (adjuster.equals("m")) {
+			value = String.valueOf(Double.parseDouble(value) * multiplier);
+		}
+		return value;
 	}
 }
