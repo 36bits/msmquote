57a5a3c6a9042cfd6505f90730c567f764fa1596
Jonathan Casiot <jonathan@pueblo.co.uk>
2022-09-09 09:53:03 +0100

    Add try-with-resources

--- src/main/java/uk/co/pueblo/msmquote/GoogleSheetsSource.java
+++ src/main/java/uk/co/pueblo/msmquote/GoogleSheetsSource.java
@@ -53,14 +53,18 @@ abstract class GoogleSheetsSource implements QuoteSource {
 	 */
 	private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
 		// Load client secrets.
+		GoogleClientSecrets clientSecrets;
 		InputStream in = GoogleSheetsSource.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-		if (in == null) {
-			throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+		try (in) {		// Try-with-resources to auto-close InputStream
+			if (in == null) {
+				throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+			}
+			clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 		}
-		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
+		
 		// Build flow and trigger user authorisation request.
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES).setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH))).setAccessType("offline").build();
+		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+				.setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH))).setAccessType("offline").build();
 		LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
 		return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
 	}
